4c8632a577afc8bc43528de5e85d69c11cffc7da
Guillermo Iruretagoyena <guillermo.iruretagoyena@gmail.com>
2012-12-26 14:12:43 +0100

    Mueve el codido de chess que estaba en testp al modulo chess.py

---
+++ chess.py
@@ -0,0 +1,63 @@
+'''
+Created on 26/12/2012
+
+@author: memo
+'''
+
+
+import cbr
+
+def letterToCol(letter):
+    letters=['a','b','c','d','e','f','g','h']
+    
+    n=0
+    for l in letters:
+        if letter==l:
+            return n
+        n=n+1
+        
+def catToInt(cat):
+    categories=['draw','zero','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen']
+    n=0
+    for c in categories:
+        if c==cat:
+            return n
+        n=n+1 
+
+
+class ChessCase(cbr.Case):
+    
+    
+    #d is a string
+    def setData(self,d):
+        
+        
+        fields=d.split(",",7)
+
+        
+        #ignore '\n'
+        fields[6]=(fields[6])[0:-1]
+        
+        
+        v=[
+        letterToCol(fields[0]),
+        int(fields[1]),
+        letterToCol(fields[2]),
+        int(fields[3]),
+        letterToCol(fields[4]),
+        int(fields[5]),
+        catToInt(fields[6]),    
+        ]
+    
+        self.data=v
+        
+
+def readChessData(name,data):   
+    file = open(name, 'r')
+    
+    line= file.readline()
+    
+    while line != '':
+        #print line
+        data.append(line)
+        line= file.readline()
--- testp.py
+++ testp.py
@@ -1,79 +1,24 @@
 
 
-import cbr
-import random
-import math
-import copy
-
-import demo
-
-
-def letterToCol(letter):
-    letters=['a','b','c','d','e','f','g','h']
-    
-    n=0
-    for l in letters:
-        if letter==l:
-            return n
-        n=n+1
-        
-def catToInt(cat):
-    categories=['draw','zero','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen']
-    n=0
-    for c in categories:
-        if c==cat:
-            return n
-        n=n+1 
+#import cbr
+#import random
+#import math
+#import copy
+#import demo
 
 
-class ChessCase(cbr.Case):
-    
-    t=0
-    
-    #d is a string
-    def setData(self,d):
-        
-        
-        fields=d.split(",",7)
-
-        
-        #ignore '\n'
-        fields[6]=(fields[6])[0:-1]
-        
-        
-        v=[
-        letterToCol(fields[0]),
-        int(fields[1]),
-        letterToCol(fields[2]),
-        int(fields[3]),
-        letterToCol(fields[4]),
-        int(fields[5]),
-        catToInt(fields[6]),    
-        ]
-    
-        self.data=v
-        
-
-def readChessData(name,data):   
-    file = open(name, 'r')
-    
-    line= file.readline()
-    
-    while line != '':
-        #print line
-        data.append(line)
-        line= file.readline()
+import chess
 
 
 #demo.numericalDemo()
 
-m= ChessCase()
+m= chess.ChessCase()
 
 data=[]
-readChessData('data/krkopt.data',data)
+chess.readChessData('data/krkopt.data',data)
 
 for da in data:
-    c=ChessCase()
+    c=chess.ChessCase()
     c.setData(da)
 
 
